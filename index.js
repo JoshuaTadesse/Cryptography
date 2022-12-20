cb040ce3647a1da89a0a2b9ede266125a6157dbf
Joshua Tadesse <eshewtadesse@gmail.com>
2022-12-20 23:01:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,90 +2,84 @@ function encrypt(key, plaintext, algorithm) {
     let output = "";
 
     switch (algorithm.value) {
-        case "AES":
+        case "AESenc":
             output = CryptoJS.AES.encrypt(plaintext, key)
             break
-        case "3DES":
+        case "3DESenc":
             output = CryptoJS.TripleDES.encrypt(plaintext, key)
             break
-        case "OTP":
+        case "OTPenc":
             output = otp(plaintext, key, "encrypt", true);
             
             break
         default:
-            console.log("No crypto selected")
-            output = "No crypto selected"
+            console.log("No encryption/decryption selected")
+            output = "No encrytion/decryption selected"
     }
     
 
-    document.getElementById("result").innerHTML = output
+    document.getElementById("view").innerHTML = output
 }
 
 function decrypt(key, cyphertext, algorithm) {
     let output = "";
 
     switch (algorithm.value) {
-        case "AES":
+        case "AESenc":
             output = CryptoJS.AES.decrypt(cyphertext, key).toString(CryptoJS.enc.Utf8)
             break
-        case "3DES":
+        case "3DESenc":
             output = CryptoJS.TripleDES.decrypt(cyphertext, key).toString(CryptoJS.enc.Utf8)
             break
-        case "OTP":
+        case "OTPenc":
             output = otp(cyphertext, key, "decrypt", true);
             break
         default:
-            console.log("No crypt option selected")
-            output = "No crypt option selected"
+            console.log("No encryption/decryption selected")
+            output = "No encryption/decryption selected"
     }
     
     if (output === "")
         output = "Wrong key"
 
-    document.getElementById("result").innerHTML = output
+    document.getElementById("view").innerHTML = output
 }
 
 let copyOutput = document.getElementById("copy")
 
 copyOutput.addEventListener("click", () => {
-    let copyText = document.getElementById('result')
+    let copyText = document.getElementById('view')
     navigator.clipboard.writeText(copyText.innerHTML)
     
     copyOutput.innerHTML = "Copied!"
     copyOutput.disabled = true
 })
 
-let paste = document.getElementById("paste")
-paste.addEventListener("click", async () => {
-    let pasteText = await navigator.clipboard.readText()
-    document.getElementById("text").value = pasteText
-})
-
 let submitButton = document.getElementById("submit")
 
-let crypt = document.getElementById("crypt")
+let crypt = document.getElementById("algo")
 
 //Change event that detects change and uses the 
 //new choice of encrypt or decrypt as a label of the button
-crypt.addEventListener("change", function(event) {
-    let newLabel = document.getElementById("crypt").value
-    submitButton.innerText = newLabel
+//crypt.addEventListener("change", function(event) {
+    //let newLabel = document.getElementById("crypt").value
+    //submitButton.innerText = newLabel
 })
 
-let cryptForm = document.getElementById('cryptForm')
+let cryptForm = document.getElementById('mainForm')
 
 cryptForm.addEventListener('submit', (event)=>{
     event.preventDefault()
 
     let key = document.getElementById("key").value
-    let text = document.getElementById("text").value
-    let algorithm = document.getElementById("algorithm")
+    let text = document.getElementById("message").value
+    let encdec = document.getElementById("encdec")
 
     if (crypt.value == 'Encrypt'){
-        encrypt(key, text, algorithm)
+        encrypt(key, text, encdec)
     }
     else if (crypt.value == "Decrypt"){
-        decrypt(key, text, algorithm)
+        decrypt(key, text, encdec)
     }
 
     copyOutput.innerHTML = 'Copy'
